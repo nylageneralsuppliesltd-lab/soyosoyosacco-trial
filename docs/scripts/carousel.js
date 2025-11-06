277e1cbcfcb2bfdc509c17d77c468f3cb2d9d57e
nylageneralsuppliesltd-lab <nylageneralsuppliesltd@gmail.com>
2025-11-06 05:35:52 +0300

    Update carousel.js

--- docs/scripts/carousel.js
+++ docs/scripts/carousel.js
@@ -21,8 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
   // === UPDATE THESE VALUES DAILY (ADJUSTED FOR LOANS AND ROA) ===
   const carouselDataWithoutROA = [
     { number: 144, description: "Total Members" },
-    { number: 902915, description: "Member Savings" },
-    { number: 233299, description: "Bank Balance" },
+    { number: 906815, description: "Member Savings" },
+    { number: 242999, description: "Bank Balance" },
     { number: 105, description: "Number of Loans Given" },
     { number: totalLoansToday, description: "Value of Loans Given" },  // Dynamically generated from loan types sum
     { number: 51803, description: "Profit" },
