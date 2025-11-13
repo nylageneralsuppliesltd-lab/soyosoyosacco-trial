e73a20a2b694283706e77fe608c56cae8e46fb12
nylageneralsuppliesltd-lab <nylageneralsuppliesltd@gmail.com>
2025-11-14 00:09:36 +0300

    Update index.js

--- docs/api/index.js
+++ docs/api/index.js
@@ -4,7 +4,6 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
-const fetch = require("node-fetch");
 const { Pool } = require("pg");
 
 const app = express();
